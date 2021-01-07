1627117647c5cf2b7f23ce567b329874f46b5025
asanvoisin <asanvoisin@rovanier.fr>
2021-01-07 10:03:11 +0100

    new script

--- src/routes/api/auth.ts
+++ src/routes/api/auth.ts
@@ -11,24 +11,24 @@ const router = Router();
 router.post('/login', (req: Request, res: Response, next) => {
   passport.authenticate('local', (error, user) => {
     if (error || !user) {
-      return res.status(404).send({error});
+      return res.status(404).send({ error });
     }
-    const token = jwt.sign({user}, 'secret');
+    const token = jwt.sign({ user }, 'secret');
     res.setHeader("token", token);
-    return res.status(200).send();
+    return res.status(200).send({ token });
   })(req, res, next)
 });
 
 // :: POST /api/users > Inscription user
 router.post('/sign-up', async (req: Request, res: Response) => {
   const hashedPassword = passwordHash.generate(req.body.password);
-  const newUser = new User();
-  newUser.uuid = uuidv4();
-  newUser.nickname = req.body.nickname;
-  newUser.email = req.body.email;
-  newUser.password = hashedPassword;
-  await getRepository(User).save(newUser);
-  return res.status(200).send();
+  const new_user = new User();
+  new_user.uuid = uuidv4();
+  new_user.nickname = req.body.nickname;
+  new_user.email = req.body.email;
+  new_user.password = hashedPassword;
+  await getRepository(User).save(new_user);
+  return res.status(200).send({ message: "Hello " + new_user.nickname });
 });
 
 export default router
